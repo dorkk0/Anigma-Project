ab1ab0909cdb7054c85a97457e70e6f1380dbd68
Dor Kriksman <dor07021991@gmail.com>
2018-11-11 15:29:36 +0200

    Need to do sanity checks + getting input for the machine settings

--- Demo.py
+++ Demo.py
@@ -9,16 +9,16 @@ profile.enable()
 
 while True:
     # enigma machine creation
-    #('Z','U'), ('H','L'), ('C','Q'), ('W','M'), ('O','A'), ('P','Y'), ('E','B'), ('T','R'), ('D','N'), ('V','I')
-    plugConfig = []
-    enigma = Enigma(1, 2, 3,  # left rotor, middle rotor, right rotor
-                    0, 0, 0,  # left setting,middle setting, right setting
-                    5, 3, 21,  # left offset , middle offset,right offset
+
+    plugConfig = [('Z','U'), ('H','L'), ('C','Q'), ('W','M'), ('O','A'), ('P','Y'), ('E','B'), ('T','R'), ('D','N'), ('V','I')]
+    enigma = Enigma(2, 5, 4,  # left rotor, middle rotor, right rotor
+                    18, 8, 23,  # left setting,middle setting, right setting
+                    ord('D') - ord('A'), ord('O') - ord('A'), ord('R') - ord('A'),  # left offset , middle offset,right offset
                     plugConfig)  # optional 10 pairs to plugboard
 
     # user interface handling
     print ('\n' * 3)
-    message = "ENIGMA"
+    message = "UMDPQ CUAQN LVVSP IARKC TTRJQ KCFPT OKRGO ZXALD RLPUH AUZSO SZFSU GWFNF DZCUG VEXUU LQYXO TCYRP SYGGZ HQMAG PZDKC KGOJM MYYDD H"
 
     #input("Enter a message to decipher (To Exit Enter-exit)\n"
     #                    "=================\n"
@@ -36,7 +36,7 @@ while True:
         encryptedMessage += enigma.encrypt(letter)
     print ("Encrypted Message:", encryptedMessage)
     print(enigma.lRotor.offset , enigma.mRotor.offset, enigma.rRotor.offset )
-
+    break
 
 profile.disable()
 print ('\n' * 10)
